29c0cc8fbce4cb384113bddad168fab3646ab0c1
emilschultz <emil.schultz@amedia.no>
2024-01-30 13:11:10 +0100

    chore: render Posts component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,24 @@
 'use client';
 import { useEffect, useState } from 'react';
 import CreatePost from './components/posts/createPost/page';
+import Posts from '../app/components/posts/posts/page';
 import styles from './page.module.css';
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    async () => {
+    (async () => {
       const getPosts = await fetch('/api/posts');
       const getPostsJson = await getPosts.json();
       setPosts(getPostsJson);
-    };
+    })();
   }, []);
 
   return (
     <main className={styles.main}>
       <CreatePost setPosts={setPosts} />
+      <Posts posts={posts} setPosts={setPosts} />
     </main>
   );
 }
